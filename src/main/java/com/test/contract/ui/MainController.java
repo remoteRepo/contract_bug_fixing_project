0df3be151bb887c6fc954ef68e2fc201e31facc2
Sporesss <LogiSt_ST@ua.fm>
2015-12-30 12:41:30 +0200

    Bug fix #3: Disabled button

--- src/main/java/com/test/contract/ui/MainController.java
+++ src/main/java/com/test/contract/ui/MainController.java
@@ -1,17 +1,24 @@
 package com.test.contract.ui;
 
+import com.sun.java.accessibility.util.EventID;
 import com.test.contract.entity.Contact;
 import com.test.contract.service.ContactService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import java.util.EventObject;
 import java.util.List;
 
 @SuppressWarnings("SpringJavaAutowiringInspection")
@@ -44,9 +51,51 @@ public class MainController {
      * который вызовется спрингом, после того, как им будут произведены все инъекции.
      * {@link MainController#init()}
      */
+
+    @FXML
+    private Button button;
+
+    boolean nameTyped = false;
+    boolean phoneTyped = false;
+    boolean emailTyped = false;
+    boolean ageTyped = false;
+
+    public void check() {
+        if (nameTyped && phoneTyped && emailTyped && ageTyped) {
+            button.setDisable(false);
+        }
+    }
+
     @FXML
     public void initialize() {
+            txtName.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) { nameTyped = true; check(); }
+            });
+            txtPhone.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) { phoneTyped = true; check(); }
+            });
+            txtEmail.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) { emailTyped = true; check(); }
+            });
+            txtAge.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) { ageTyped = true; check(); }
+            });
+            button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    nameTyped = false;
+                    phoneTyped = false;
+                    emailTyped = false;
+                    ageTyped = false;
+                    button.setDisable(true);
+                }
+            });
     }
+
 
     /**
      * На этом этапе уже произведены все возможные инъекции.
