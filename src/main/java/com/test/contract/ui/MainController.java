3440776bf20b623d73958dbf5ca1ab9529ccad19
dmitro <test_git@mail.ru>
2016-01-02 18:44:33 +0200

    short construction IF by inversion, forget ! near txtAge

--- src/main/java/com/test/contract/ui/MainController.java
+++ src/main/java/com/test/contract/ui/MainController.java
@@ -88,7 +88,7 @@ public class MainController {
         String removedEmail = txtEmail.getText().trim();
         String removedAge = txtAge.getText().trim();
 
-        if(!(removedName.equals("")) && !(removedPhone.equals("")) && !(removedEmail.equals("")) && (removedAge.equals(""))) {
+        if(!(removedName.equals("")) && !(removedPhone.equals("")) && !(removedEmail.equals("")) && !(removedAge.equals(""))) {
             contactService.save(contact);
             data.add(contact);
             txtName.setText("");
